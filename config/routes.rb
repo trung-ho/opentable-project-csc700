fe9670aa1b2875d5df2d8f6adbaa975a6c1cdddc
Michael Ho <chacharoti@gmail.com>
2019-03-01 23:17:58 -0800

    Update style

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#index'
+
+  get 'restaurant/show/' => 'application#show'
 end
